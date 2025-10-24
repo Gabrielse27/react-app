99e74406a035b22d3d67763bb1e22173fc2e9cdb
Your Name <you@example.com>
2025-10-24 10:39:02 +0200

    Add About section: layout, copy och grund-styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,14 @@
- import React from 'react'
- 
- const Button = () => {
-   return (
-     <button className='btn btn-primary'>Button</button>
-   )
- }
- 
- export default Button
+ type ButtonProps = {
+  text: string;
+  onClick?: () => void;
+};
+
+const Button = ({ text, onClick }: ButtonProps) => {
+  return (
+    <button className="btn btn-warning px-4 py-2 rounded" onClick={onClick}>
+      {text}
+    </button>
+  );
+};
+<Button text="Get Started" />
+export default Button;
