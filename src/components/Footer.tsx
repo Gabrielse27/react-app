c6e5820ad07ef8354a752d919b32062d4b8d7bd6
Your Name <you@example.com>
2025-11-14 14:50:52 +0100

    ok chevron avstånd

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
  import React from "react";
- import { FaChevronCircleRight } from "react-icons/fa";
+ import {FaChevronRight} from "react-icons/fa6";
 import "./footer.css"; 
 
 const Footer = () => {
@@ -11,13 +11,13 @@ const Footer = () => {
 
         <ul className="footer-terms">
 
-            <li>< FaChevronCircleRight aria-hidden="true"/> 
+            <li><FaChevronRight aria-hidden="true"/> 
             <a href="/true" aria-label="Read our Terms and Conditions">
             Terms & Conditions</a>
             </li>
 
 
-            <li>< FaChevronCircleRight aria-hidden="true"/> 
+            <li><FaChevronRight aria-hidden="true"/> 
             <a href="/policy" aria-label="Read Privacy Policy Conditions">
             Privacy Policy</a>
             </li>
